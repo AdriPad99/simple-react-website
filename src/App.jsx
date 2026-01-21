8226e6b8408d0626d9db16463cb26e8760fe4869
Adrian P <adrian.mpadilla99@gmail.com>
2026-01-20 21:37:06 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React</h1>
+        <p>
+          first-react-website
+        </p>
+      </header>
+      <main>
+        <h2>Lorem ipsum dolor</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
